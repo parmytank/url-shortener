01262f199e6889949bb5a2dc256979002297d1cf
armytank <p.armytank@gmail.com>
2016-07-18 13:03:34 +0000

    added database

---
+++ server.js
@@ -0,0 +1,16 @@
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGOLAB_URI;
+
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
